b2d196f8b37ba56ea30ef402162c02fda1856222
wrslatz <wrslatz@gmail.com>
2026-01-25 23:48:07 -0500

    feat: improve aliases for brew commands

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,5 +1,7 @@
 alias ls='ls --color' # list with color
 alias ll='ls -ltraF'  # list all
-alias update_tools='sudo apt update && sudo apt upgrade && sudo apt autoremove && brew update && (brew bundle check --global || brew bundle install --global) && brew cleanup && omz update'
+alias update_tools='sudo apt update && sudo apt upgrade && sudo apt autoremove && update_brew && omz update'
 alias update_os='sudo apt dist-upgrade && sudo do-release-upgrade -d'
 alias src="source ~/.zshrc"
+alias backup_brew="brew bundle dump --global --describe --force"
+alias update_brew="brew update && (brew bundle check --global || brew bundle install --global) && brew cleanup"
