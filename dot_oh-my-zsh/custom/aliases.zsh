c30e80f3e645f63b2788fef94a2daee9eba77ec5
wrslatz <wrslatz@gmail.com>
2026-02-06 17:34:06 -0500

    chore: improve brew update aliases

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,9 @@
 alias ls='ls --color' # list with color
 alias ll='ls -ltraF'  # list all
-alias update_tools='sudo apt update && sudo apt upgrade && sudo apt autoremove && update_brew && omz update'
+alias sync_brew="chezmoi update --apply=false && chezmoi apply ~/.Brewfile"
+alias backup_brew="brew bundle dump --global --describe --force && chezmoi re-add ~/.Brewfile"
+alias update_brew="brew update && (brew bundle check --global || brew bundle install --global) && brew cleanup"
+alias update_tools='sudo apt update && sudo apt upgrade && sudo apt autoremove && sync_brew && update_brew && backup_brew && omz update'
 alias update_os='sudo apt dist-upgrade && sudo do-release-upgrade -d'
 alias src="source ~/.zshrc"
 alias backup_brew="brew bundle dump --global --describe --force"
