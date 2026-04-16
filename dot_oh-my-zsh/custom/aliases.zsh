4bdbc852d47227bf2234c65698f775ebe7c8252f
wrslatz <wrslatz@gmail.com>
2026-04-16 15:58:10 -0400

    feat: improve tool updating

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,10 +1,19 @@
+check_chezmoi_git_status(){
+    if [ -n "$(chezmoi git -- status --porcelain)" ]; then
+        echo "Error: chezmoi working directory is not clean. Please commit or stash changes before syncing chezmoi."
+        chezmoi git -- --no-pager diff
+        return 1
+    fi
+    return 0
+}
+
 alias ls='ls --color' # list with color
 alias ll='ls -ltraF'  # list all
-alias sync_brew="chezmoi update --apply=false && chezmoi apply ~/.Brewfile"
-alias backup_brew="brew bundle dump --global --describe --force && chezmoi re-add ~/.Brewfile"
-alias update_brew="brew update && (brew bundle check --global || brew bundle install --global) && brew upgrade && brew cleanup"
-alias update_tools='sudo apt update && sudo apt upgrade && sudo apt autoremove && sync_brew && update_brew && backup_brew && omz update'
+alias sync_brew='printf "Updating and applying ~/.Brewfile...\n" && chezmoi update --apply=false && chezmoi apply ~/.Brewfile && printf "~/.Brewfile updated and applied!\n"'
+alias backup_brew='printf "Backing up brew to ~/.Brewfile and syncing w/ chezmoi...\n" && brew bundle dump --global --describe --force && chezmoi re-add ~/.Brewfile && printf "brew backup complete!\n"'
+alias update_brew='printf "Updating brew and dependencies...\n" && brew update && (brew bundle check --global || brew bundle install --global) && brew upgrade && brew cleanup && printf "brew and dependencies updated!\n"'
+alias diff_chezmoi="chezmoi git pull && check_chezmoi_git_status && chezmoi diff"
+alias sync_chezmoi='printf "Diffing and syncing chezmoi...\n" && diff_chezmoi && chezmoi update && printf "chezmoi updated!\n"'
+alias update_tools='printf "Updating tools...\n" && sudo apt update && sudo apt upgrade && sudo apt autoremove && sync_chezmoi && update_brew && omz update && printf "All tools updated!"'
 alias update_os='sudo apt dist-upgrade && sudo do-release-upgrade -d'
 alias src="source ~/.zshrc"
-alias backup_brew="brew bundle dump --global --describe --force"
-alias update_brew="brew update && (brew bundle check --global || brew bundle install --global) && brew cleanup"
