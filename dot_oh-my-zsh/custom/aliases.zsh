dc2247523ec2e6014e9e5ccc9d8605f932a27c06
wrslatz <wrslatz@gmail.com>
2026-04-16 15:59:32 -0400

    chore: use printf instead of echo

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -14,6 +14,6 @@ alias backup_brew='printf "Backing up brew to ~/.Brewfile and syncing w/ chezmoi
 alias update_brew='printf "Updating brew and dependencies...\n" && brew update && (brew bundle check --global || brew bundle install --global) && brew upgrade && brew cleanup && printf "brew and dependencies updated!\n"'
 alias diff_chezmoi="chezmoi git pull && check_chezmoi_git_status && chezmoi diff"
 alias sync_chezmoi='printf "Diffing and syncing chezmoi...\n" && diff_chezmoi && chezmoi update && printf "chezmoi updated!\n"'
-alias update_tools='printf "Updating tools...\n" && sudo apt update && sudo apt upgrade && sudo apt autoremove && sync_chezmoi && update_brew && omz update && printf "All tools updated!"'
+alias update_tools='printf "Updating tools...\n" && sudo apt update && sudo apt upgrade && sudo apt autoremove && diff_chezmoi && sync_chezmoi && update_brew && backup_brew && omz update && sync_chezmoi && printf "All tools updated!"'
 alias update_os='sudo apt dist-upgrade && sudo do-release-upgrade -d'
 alias src="source ~/.zshrc"
